7c527b891120b3053bc87693fa5f594637cd3635
Bas Hepping <bashepping@rapide.nl>
2015-05-07 15:36:17 +0200

    Nested rows

--- src/Repositories/PageRepository.php
+++ src/Repositories/PageRepository.php
@@ -25,7 +25,8 @@ class PageRepository extends AbstractRepository
                 $html .= '<div class="' . $column['class'] . '">';
                 $html .= '<div class="xpo_data">';
                 $html .= (isset($column['template_name']) && strlen($column['template_name']) > 0) ? '<xpodata data-templatename="' . $column['template_name'] . '"></xpodata>' : "";
-                $html .= (isset($column['template_name']) && strlen($column['template_name']) > 0) ? TemplateParser::parseForInput($column['template_name'], $column['template_data']) : "<button type='button' class='btn btn-primary btn-block btn-select-template'>Select template</button>";
+                $html .= (isset($column['template_name']) && strlen($column['template_name']) > 0) ? TemplateParser::parseForInput($column['template_name'],
+                    $column['template_data']) : "<button type='button' class='btn btn-primary btn-block btn-select-template'>Select template</button>";
                 $html .= '</div>';
                 $html .= '</div>';
             }
@@ -45,29 +46,7 @@ class PageRepository extends AbstractRepository
         $parsedForDatabase = [];
         foreach ($template_array as $key => $row) {
             // Row
-            $parsedForDatabase[$key] = [
-                "class"   => "NA",
-                "columns" => []
-            ];
-            foreach ($row as $col_key => $column) {
-                // Column[0] = length
-                // Column[1] = Template name
-                // Column[2] = Fields
-                // Column[3] = Subnodes
-                $input = [];
-                foreach ($column[2] as $field) {
-                    $input[$field] = Input::get($field);
-                    if (Request::hasFile($field . ".file")) {
-                        $input[$field]['file'] = Request::file($field . ".file");
-                    }
-                }
-
-                $parsedForDatabase[$key]['columns'][$col_key] = [
-                    'class'         => trim($column[0]),
-                    'template_name' => (!isset($column[1]) || is_null($column[1]) ? "" : trim($column[1])),
-                    'template_data' => (!isset($column[1]) || is_null($column[1]) ? "{}" : TemplateParser::parseForDatabase($column[1], $input))
-                ];
-            }
+            $parsedForDatabase = $this->createArrayFromData($key, $row);
         }
 
         $json_parsed_data = json_encode($parsedForDatabase,
@@ -84,5 +63,51 @@ class PageRepository extends AbstractRepository
     public function beforeUpdate($template_array = [])
     {
         return $this->beforeCreate($template_array);
+    }
+
+    /**
+     * @param $parsedForDatabase
+     * @param $key
+     * @param $row
+     *
+     * @return mixed
+     */
+    protected function createArrayFromData($key, $row)
+    {
+        $parsedForDatabase = [];
+        $parsedForDatabase[$key] = [
+            "class"   => "NA",
+            "columns" => []
+        ];
+        foreach ($row as $col_key => $column) {
+            // Column[0] = length
+            // Column[1] = Template name
+            // Column[2] = Fields
+            // Column[3] = Subnodes
+            $input = [];
+            foreach ($column[2] as $field) {
+                $input[$field] = Input::get($field);
+                if (Request::hasFile($field . ".file")) {
+                    $input[$field]['file'] = Request::file($field . ".file");
+                }
+            }
+
+            $parsedRow = [];
+            if (isset($column[3]) && count($column[3]) > 0) {
+                foreach ($column[3] as $row_key => $row_data) {
+                    $parsedRow = $this->createArrayFromData($row_key, $row_data);
+                }
+            }
+
+            $parsedForDatabase[$key]['columns'][$col_key] = [
+                'class'         => trim($column[0]),
+                'template_name' => (!isset($column[1]) || is_null($column[1]) ? "" : trim($column[1])),
+                'template_data' => (!isset($column[1]) || is_null($column[1]) ? "{}" : TemplateParser::parseForDatabase($column[1],
+                    $input)),
+                'nested_rows'   => $parsedRow
+            ];
+        }
+
+        return $parsedForDatabase;
     }
 }
