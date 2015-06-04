604b5fdd80563f62dd5fe77c22ab50bbe40abb59
Bas Hepping <bashepping@rapide.nl>
2015-06-04 11:41:45 +0200

    Automatic creation of a slug

--- src/views/pages/_form.blade.php
+++ src/views/pages/_form.blade.php
@@ -141,3 +141,19 @@
         </div>
     </div>
 </div>
+
+@section('scripts')
+    @parent
+    <script>
+        $(document).ready(function() {
+            $('#name').blur(function() {
+                $('#slug').val("/" + $(this).val().toLowerCase()
+                        .replace(/\s+/g, '-')           // Replace spaces with -
+                        .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+                        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+                        .replace(/^-+/, '')             // Trim - from start of text
+                        .replace(/-+$/, ''));            // Trim - from end of text
+            });
+        });
+    </script>
+@stop
