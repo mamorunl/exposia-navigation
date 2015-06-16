26f0e04023db1e8db2d85215d0ce71099ef882b9
Bas Hepping <bashepping@rapide.nl>
2015-06-16 11:53:13 +0200

    Added SEO migration

---
+++ src/database/migrations/2015_06_16_094119_add_seo_to_pages.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddSeoToPages extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('cms_pages', function(Blueprint $table)
+		{
+			$table->string('meta_keywords');
+            $table->text('meta_description');
+            $table->string('seo_title');
+            $table->boolean('include_in_sitemap');
+            $table->string('robots_index');
+            $table->string('robots_follow');
+            $table->string('canonical_url');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('cms_pages', function(Blueprint $table)
+		{
+            $table->dropColumn('meta_keywords');
+            $table->dropColumn('meta_description');
+            $table->dropColumn('seo_title');
+            $table->dropColumn('include_in_sitemap');
+            $table->dropColumn('robots_index');
+            $table->dropColumn('robots_follow');
+            $table->dropColumn('canonical_url');
+		});
+	}
+
+}
