4c0c2735ea6a69653d5582cb7f7dba8ec28ed063
Bas Hepping <bashepping@rapide.nl>
2015-06-18 11:39:32 +0200

    Changed name of migration

--- src/database/migrations/2015_03_12_162045_create_cms_pages_table.php
+++ src/database/migrations/2015_03_12_162045_create_cms_pages_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCMSPagesTable extends Migration {
+class CreateCmsPagesTable extends Migration {
 
 	/**
 	 * Run the migrations.
