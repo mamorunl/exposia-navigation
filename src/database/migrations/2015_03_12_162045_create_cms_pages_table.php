d66aad1473e3f249ab2a65bb8c104d113bd8c8be
Bas Hepping <bashepping@rapide.nl>
2015-06-18 11:56:01 +0200

    Changed name of migration

--- src/database/migrations/2015_03_12_162045_create_cms_pages_table.php
+++ src/database/migrations/2015_03_12_162045_create_cms_pages_table.php
@@ -3,33 +3,30 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCmsPagesTable extends Migration {
+class CreateCmsPagesTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('cms_pages', function(Blueprint $table)
-		{
-			$table->increments('id');
+    /**
+     * Run the migrations.
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cms_pages', function (Blueprint $table) {
+            $table->increments('id');
             $table->string('title');
             $table->text('template_data');
             $table->integer('node_id');
-			$table->timestamps();
-		});
-	}
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('cms_pages');
-	}
-
+    /**
+     * Reverse the migrations.
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cms_pages');
+    }
 }
