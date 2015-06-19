47ee2b64ef7881b2e51d6454f99c5f42b4b2adb5
Bas Hepping <bashepping@rapide.nl>
2015-06-19 12:04:10 +0200

    Added a forced display to the selectpicker

--- src/resources/views/parsers/fonticon_parser/form.blade.php
+++ src/resources/views/parsers/fonticon_parser/form.blade.php
@@ -1,6 +1,10 @@
 <input type="hidden" value="{{ $xpo_id }}" name="{{ $key }}[xpo_id]">
-<select class="selectpicker" name="{{ $key }}[value]">
+<select class="selectpicker" name="{{ $key }}[value]" id="{{ $key }}">
     @foreach($icons as $name => $hex)
         <option value="fa {{ $name }}" @if($values['value'] == "fa " . $name)selected @endif data-icon="fa {{ $name }}"> {{ $name }}</option>
     @endforeach
 </select>
+
+<script>
+    $('#{{ $key }}').selectpicker();
+</script>
